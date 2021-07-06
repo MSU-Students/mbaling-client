c1a02a04a45254699d9fd6ba533f101acbe1eb25
fencim <fencim@gmail.com>
2021-07-06 14:50:20 +0800

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,45 @@
+<template>
+ <div >
+    <h1>{{1+1}}</h1>
+     <q-btn color="grey-4" text-color="purple" glossy unelevated icon="camera_enhance" label="Purple text" />
+    <q-btn to="/login">GO to Login</q-btn>
+    <q-btn v-for="todo in todos" :key="todo.id">{{todo.content}}</q-btn>
+ </div>
+</template>
+
+<script lang="ts">
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ClassComponent.vue';
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  components: { ExampleComponent }
+})
+export default class PageIndex extends Vue {
+  todos: Todo[] = [
+    {
+      id: 1,
+      content: 'Pamipi'
+    },
+    {
+      id: 2,
+      content: 'Pamaypas'
+    },
+    {
+      id: 3,
+      content: 'Pelamapaso'
+    },
+    {
+      id: 4,
+      content: 'Pangaroma'
+    },
+    {
+      id: 5,
+      content: 'Paninimo'
+    }
+  ];
+  meta: Meta = {
+    totalCount: 1200
+  };
+};
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h6>THis is login page</h6>
+    </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class LoginPage extends Vue {
+}
+</script>
